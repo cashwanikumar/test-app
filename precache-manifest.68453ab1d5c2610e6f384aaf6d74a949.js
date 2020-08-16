0a974e5d5ba6b40cce3b6db77ecdd032f42ece72
Ashwani <ashwani@illuminate.ae>
2020-08-17 01:24:01 +0530

    init commit

---
+++ precache-manifest.68453ab1d5c2610e6f384aaf6d74a949.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14eff953ad1eb58c8b60c340e9a55c43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62fc892fe7738211a78c",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0356c7e6019d9e84ad67",
+    "url": "/static/css/main.026b6018.chunk.css"
+  },
+  {
+    "revision": "62fc892fe7738211a78c",
+    "url": "/static/js/2.c989b3ed.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/static/js/2.c989b3ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0356c7e6019d9e84ad67",
+    "url": "/static/js/main.0b3ab5bb.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
